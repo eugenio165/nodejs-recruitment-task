b8f19c85e5df3c89b263d815b263b68d8ff8ef84
Antônio Eugênio <eugenio165.as@gmail.com>
2022-03-29 13:35:43 -0300

    movies: removing default files

--- movies/src/app.module.ts
+++ movies/src/app.module.ts
@@ -1,17 +1,13 @@
+import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './config/app-config.module';
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
     AppConfigModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
